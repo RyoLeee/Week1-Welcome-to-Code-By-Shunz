e1bad154f7591748b5b6ef063b19c0e0b3cc32f7
RyoLeee <h4285007@gmail.com>
2025-08-25 09:50:21 +0700

    SOAL 2 DONE

---
+++ quiz/part2/soalpart2.js
@@ -0,0 +1,44 @@
+// SOAL 1
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+
+console.log(' ');
+
+// SOAL 2
+let word1 = 'wow JavaScript is so cool';
+let exampleFirstWord = word1[0] + word1[1] + word1[2];
+let second1 = word1[4] + word1[5] + word1[6] + word1[7] + word1[8] + word1[9] + word1[10] + word1[11] + word1[12] + word1[13];
+let third1 = word1[15] + word1[16];
+let fourth1 = word1[18] + word1[19];
+let fifth1 = word1[20] + word1[21] + word1[22] + word1[23] + word1[24];
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + second1);
+console.log('Third Word: ' + third1);
+console.log('Fourth Word: ' + fourth1);
+console.log('Fifth: ' + fifth1);
+
+console.log(' ');
+
+// SOAL 3
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let second2 = word3.substring(4, 14);
+let third2 = word3.substring(15, 17);
+let fourth2 = word3.substring(18, 20);
+let fifth2 = word3.substring(20, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + second2);
+console.log('Third Word: ' + third2);
+console.log('Fourth Word: ' + fourth2);
+console.log('Fifth word: ' + fifth2);
+
+// DONE
